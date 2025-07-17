3fb4d3387b7c32008f0edf11762990411c0016bb
Ankit Tripathi <ankitdhonee@gmail.com>
2025-07-17 17:26:57 +0530

    final version

--- src/hooks/useNavbarColor.jsx
+++ src/hooks/useNavbarColor.jsx
@@ -1,25 +1,24 @@
 import { useEffect, useState } from "react";
 
 const useNavbarColor = () => {
-    const [navbarColor, setNavbarColor] = useState(false);
+  const [navbarColor, setNavbarColor] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 10) {
-                setNavbarColor("black");
-            }else{
-                setNavbarColor("#131110");
-            }
-        };
-        window.addEventListener("scroll", handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setNavbarColor("black");
+      } else {
+        setNavbarColor("#131110");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    }, []);
-
-    return navbarColor;
+  return navbarColor;
 };
 
 export default useNavbarColor;
--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -2,21 +2,24 @@ import { HiShieldCheck } from "react-icons/hi";
 import { MdCancel, MdAnalytics } from "react-icons/md";
 
 const data = [
-    {
-        icon: <HiShieldCheck />,
-        heading: "Best interest rates on the market",
-        detail: "Discover Properties with the most competitive interest rates available, enabling you to save significantly over the life of your investment through lower mortgage payments & reduced overall financing costs.",
-    },
-    {
-        icon: <MdCancel />,
-        heading: "Prevent unstable prices",
-        detail: "Explore Properties in markets equipped with measures to prevent unstable price fluctuations, providing peace of mind & long-term security in your investment by mitigating the risks associated with volatile real estate markets.",
-    },
-    {
-        icon: <MdAnalytics />,
-        heading: "Best price on the market",
-        detail: "Find your ideal Property at the best price on the market, ensuring you receive maximum value for your investment & enabling you to capitalize on opportunities for growth & profitability in the real estate sector.",
-    },
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates on the market",
+    detail:
+      "Discover Properties with the most competitive interest rates available, enabling you to save significantly over the life of your investment through lower mortgage payments & reduced overall financing costs.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Explore Properties in markets equipped with measures to prevent unstable price fluctuations, providing peace of mind & long-term security in your investment by mitigating the risks associated with volatile real estate markets.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Find your ideal Property at the best price on the market, ensuring you receive maximum value for your investment & enabling you to capitalize on opportunities for growth & profitability in the real estate sector.",
+  },
 ];
 
 export default data;
